8d726dc0b6fc4d6114731c857066a6fcfad6e631
123abcmeow <21meow141meow141@gmail.com>
2025-09-15 05:25:56 +0000

    wowow

--- week2/temp.c
+++ week2/temp.c
@@ -2,9 +2,9 @@
 int main(void){
 
     float iTempC , iTempF;
-    printf("enter temperature(Celsius):");
+    printf("Enter temperature(Celsius):");
     scanf("%f",&iTempC);
     iTempF = iTempC * 9 / 5 +32;
-    printf("Temperature converts to Fahrenheit is :%.2fF hehe\n",iTempF);
+    printf("%fC converts to Fahrenheit is :%.2fF hehe\n",iTempC,iTempF);
 
 }//end main
