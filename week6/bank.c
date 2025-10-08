a7104075d02170f226f511c3a4ea69eb6e0e6b6c
123abcmeow <21meow141meow141@gmail.com>
2025-10-08 17:10:54 +0000

    done90%

--- week6/bank.c
+++ week6/bank.c
@@ -2,28 +2,50 @@
 int main(void){
     
     int ibalance = 10000;
-    int iwithdraw = 0;
+    int iwithdraw = -1;
+    int ipassword = 0;
+    int ienter = 0;
     printf("Balance setting:10000\n");
     printf("Current Balance: %d\n",ibalance);
+
+    printf("Enter your 4 digit password:");
+    scanf("%d",&ipassword);
+    while(ipassword < 1000 || ipassword > 9999){
+        printf("Password must be 4 digits!!!:");
+        scanf("%d",&ipassword);
+    }
     
-    while(ibalance > 0){
+    while(iwithdraw != 0){
         printf("Enter Withdraw Amount(or 0 to exit):");
         scanf("%d",&iwithdraw);
-        if(iwithdraw % 100 != 0){
+        if(iwithdraw < 0){
+            printf("Invalid Amount\n");
+        }
+        else if(iwithdraw % 100 != 0){
             printf("Withdrawal amount must be a multiple of 100\n");
         }
         else if(iwithdraw > ibalance){
             printf("Insufficient Balance\n");
         }
-        else if(iwithdraw <= ibalance && iwithdraw % 100 == 0){
-            ibalance -= iwithdraw;
-            printf("Withdraw Amount: %d\n",iwithdraw);
-            printf("Current Balance: %d\n",ibalance);
+        else if(iwithdraw % 100 == 0 && iwithdraw != 0){
+            while(ienter != ipassword){
+                printf("Enter Password(4 digits):");
+                scanf("%d",&ienter);
+                if(ienter != ipassword){
+                    printf("Wrong Password\n");
+                }
+                else if(ienter == ipassword){
+                    ibalance -= iwithdraw;
+                    printf("Withdraw Amount: %d\n",iwithdraw);
+                    printf("Current Balance: %d\n",ibalance);
+                    ienter = 0;
+                    break;
+                }
+            }
         }
-        else if (iwithdraw == 0){
-            printf("Exit\n");
-            break;
-        }
+    }
+    if(iwithdraw == 0){
+        printf("transaction ended.\n");
     }
 
 }
