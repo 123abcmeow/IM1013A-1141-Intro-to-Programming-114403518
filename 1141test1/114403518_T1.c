7864f05850b5321cb3224e47988a353a0a32f11f
123abcmeow <21meow141meow141@gmail.com>
2025-10-17 03:17:44 +0000

    1231213

--- 1141test1/114403518_T1.c
+++ 1141test1/114403518_T1.c
@@ -4,27 +4,37 @@ int main(void){
     int iadult = 0;
     int istudent = 0;
     int itotal = 0;
-    int 200x = 0;
-    int 150x = 0;
-    int isum = 0;
+    float fsum = 0;
     int ismult5 = 0;
 
-    printf("1) Option 1 - Single-Order Quatation");
-    printf("2) Option 2 - Group Total & Installments");
+    printf("\n==== Future City Expo Ticket Systems ====\n");
+    printf("1) Option 1 - Single-Order Quatation\n");
+    printf("2) Option 2 - Group Total & Installments\n");
     printf("(-1) End Program\n");
     printf("Select:");
     scanf("%d",&ioption);
+    if(ioption == -1){
+        printf("Program ended.\n");
+    }
 
     while(ioption != -1){
-        while(ioption != -1 || ioption != 1 || ioption != 2){
+        while(ioption >2 || ioption < 1){
             printf("Unknown Selection, Please Enter 1, 2, or -1 to Exit:\n");
             scanf("%d",&ioption);
         }
         if(ioption == 1){
             printf("[option 1] Enter number of adults(1-20):");
             scanf("%d",&iadult);
-            printf("[option 1] Enter number of students:(1-20)");
+            if(iadult == -1){
+                printf("Program ended.\n");
+                break;
+            }
+            printf("[option 1] Enter number of students:(1-20):");
             scanf("%d",&istudent);
+            if(istudent == -1){
+                printf("Program ended.\n");
+                break;
+            }
             itotal = iadult + istudent;
             while(itotal < 1 || itotal > 20){
                 printf("Total number of people must be between 1 and 20:\n");
@@ -34,35 +44,39 @@ int main(void){
                 scanf("%d",&istudent);
                 itotal = iadult + istudent;
             }
-            isum = iadult * 200 + istudent * 150;
+            fsum = (float)iadult * 200 + (float)istudent * 150;
 
+            printf("\n--- Quatatioon Summary ---\n");
             printf("Adults:%d, Students:%d, Total Tickets:%d\n",iadult,istudent,itotal);
-            printf("Base Total: %d\n",isum);
+            printf("Base Total: %2.f\n",fsum);
 
             if(itotal >= 1 && itotal <= 5){
                 printf("Quantity Discount Factor:No\n");
             }
             else if(itotal >= 6 && itotal <= 10){
                 printf("Quantity Discount Factor:0.95\n");
-                isum = isum * 0.95;
+                fsum = fsum * 0.95;
             }
             else if(itotal >= 11 && itotal <= 15){
                 printf("Quantity Discount Factor:0.9\n");
-                isum = isum * 0.9;
+                fsum = fsum * 0.9;
             }
             else if(itotal >= 16 && itotal <= 20){
                 printf("Quantity Discount Factor:0.85\n");
-                isum = isum * 0.85;
+                fsum = fsum * 0.85;
             }
             if(itotal % 5 == 0){
-                isum = isum * 0.95;
-                printf("Multiple-of-5 Discount Applied:Yes\n");
-                printf("Final Payable Amount:%d\n",isum);
+                fsum = fsum * 0.95;
+                printf("Multiple-of-5 Discount Applied:Yes (Factor 0.95)\n");
+                printf("Final Payable Amount:%2.f\n",fsum);
             }
             else{
                 printf("Multiple of 5 Discount Applied:No\n");
-                printf("Final Payable Amount:%d\n",isum);
+                printf("Final Payable Amount:%2.f\n",fsum);
             }
+        }
+        if(ioption == 2){
+            printf("[option 2] Enter");
         }
     }       
 
