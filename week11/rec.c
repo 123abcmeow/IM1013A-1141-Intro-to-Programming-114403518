5a51c9fe1bbc0ad72ddd6723bc058d05d28bb9d0
123abcmeow <21meow141meow141@gmail.com>
2025-11-14 05:17:10 +0000

    ee

--- week11/rec.c
+++ week11/rec.c
@@ -2,12 +2,20 @@
 #include <math.h>
 int main(void){
     int descend(int);
-    int number = 8;
-    int result = descend(number);
+    int number, result;
+    
+    printf("Enter a number between 1 to 9:");
+    scanf("%d", &number);
+    while(number < 1 || number > 9){
+        printf("Invalid number! Enter a number between 1 to 9:");
+        scanf("%d", &number);
+    }
+    result = descend(number);
     printf("The descending number is: %d\n", result);
+    
 }//end main
 int descend(int num){
-    if(num = 1){
+    if(num == 1){
         return num;
     }
     else{
