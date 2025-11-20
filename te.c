9d56bf57eeb41ac5f48e0a8337abdf5ada34f3f5
123abcmeow <21meow141meow141@gmail.com>
2025-11-20 16:43:44 +0000

    123

--- te.c
+++ te.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
-#include <math.h>
-int down(int);
+
 int main(void){
-    int x = -2;
-    float result;
-    int num = 1;
-    float result2;
-    printf("Enter a non-negative integer:");
-    scanf("%d",&num);
-    while(num<0){
-        printf("Invalid input! Enter a non-negative integer:");
-        scanf("%d",&num);
-    }
-    num = num*2;
-    int s = -1;
-    for(int i=0; i<num; i+=2){
-        result = pow(x,i)/down(i)*s;
-        s *= -1;
-        result2 -= result;
-        printf("f(x) = %f\n",result2);
-    }
-}//end main
-int down(int q){
-    if(q==0){
-        return 1;
-    }
-    else{
-        return q*down(q-1);
+    int w, h, t;
+
+    printf("Width: ");
+    scanf("%d", &w);
+
+    printf("Height: ");
+    scanf("%d", &h);
+
+    printf("Border thickness: ");
+    scanf("%d", &t);
+
+    for(int r = 0; r < h; r++){
+        for(int c = 0; c < w; c++){
+            if(r < t || r >= h - t || c < t || c >= w - t){
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
     }
 }
